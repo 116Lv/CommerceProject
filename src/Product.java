7f9e46c62f395d792c252b1a42441ebe450b0a51
116_Lv <lbw0109@gmail.com>
2026-03-26 19:07:05 +0900

    Step-1 완성

--- src/Product.java
+++ src/Product.java
@@ -4,11 +4,26 @@ public class Product {
     String prodContent; // 설명
     int leftCnt;        // 재고수량
 
-    public Product(String prodName, int price, String prodContent, int leftCnt) {
+    public Product(String prodName, int price, String prodContent) {
         this.prodName = prodName;
         this.price = price;
         this.prodContent = prodContent;
-        this.leftCnt = leftCnt;
+        this.leftCnt = 0;
     }
 
+    public String getProdName() {
+        return this.prodName;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public String getProdContent() {
+        return this.prodContent;
+    }
+
+    public String getPrintInfo() {
+        return String.format("%-15s| %,10d원 | %s", prodName, price, prodContent);
+    }
 }
